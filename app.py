3d33e7d839461dc0efd4a86d03b9daf07ca3f782
scriptstreamvrpc <vrspace2025@gmail.com>
2025-08-18 16:10:27 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+# route untuk asset glb atau media streaming
+@app.route("/assets/<path:filename>")
+def assets(filename):
+    return send_from_directory("static/assets", filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
